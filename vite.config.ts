700046f59f61dd2f1bdcc935788b90478a3481c8
Ruhiddin <niddihur@gmail.com>
2026-02-24 09:09:42 +0500

    fix: vite config figured out

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   // IMPORTANT: replace __REPO_NAME__ with your GitHub repo name
-  base: '/__REPO_NAME__/',
+  base: "/simple-math/",
   server: {
     port: 5173,
   },
