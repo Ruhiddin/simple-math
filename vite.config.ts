06ec87d2df7b18600d2f31e6ebdce6a43d2bbe97
Ruhiddin Turdaliyev <108495711+Ruhiddin@users.noreply.github.com>
2026-02-24 07:10:28 +0500

    Prepare Vite app config for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  // IMPORTANT: replace __REPO_NAME__ with your GitHub repo name
+  base: '/__REPO_NAME__/',
   server: {
     port: 5173,
   },
